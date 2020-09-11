289d05de13b381bb29bb307a7a1ca53e034a20dc
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-09-11 09:01:29 +0300

    Initial Commit

---
+++ PageViewControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageViewControllerDemo
+//
+//  Created by Ермоленко Константин on 11.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
