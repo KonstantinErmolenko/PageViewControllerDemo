5fc4c46bf6d69ba42d65b27b2a122ed41b2d369e
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-09-11 09:44:04 +0300

    Настроил отображение 5 страниц случайного цвета. Циклическая горизонтальная прокрутка страниц. Без индикации.

--- PageViewControllerDemo/ViewController.swift
+++ PageViewControllerDemo/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    var pageController: UIPageViewController!
+    var controllers = [UIViewController]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        layoutUI()
+        populateControllers()
+    }
+    
+    func layoutUI() {
+        // Page Controller
+        pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageController.delegate   = self
+        pageController.dataSource = self
+        addChild(pageController)
+        view.addSubview(pageController.view)
+        
+        // Constraints
+        let views = ["pageController": pageController.view] as [String: AnyObject]
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[pageController]|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[pageController]|", options: [], metrics: nil, views: views))
+    }
+    
+    func populateControllers() {
+        for _ in 1...5 {
+            let vc = UIViewController()
+            vc.view.backgroundColor = randomColor()
+            controllers.append(vc)
+        }
+        
+        pageController.setViewControllers([controllers[0]], direction: .forward, animated: true)
+    }
+    
+    // MARK: - DataSource Methods
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.firstIndex(of: viewController) {
+            if index > 0 {
+                return controllers[index - 1]
+            } else {
+                return controllers.last
+            }
+        }
+
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.firstIndex(of: viewController) {
+            if index < controllers.count - 1 {
+                return controllers[index + 1]
+            } else {
+                return controllers.first
+            }
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Supporting Methods
+    func randomCGFloat() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
     }
 
-
+    func randomColor() -> UIColor {
+        return UIColor(red: randomCGFloat(), green: randomCGFloat(), blue: randomCGFloat(), alpha: 1)
+    }
 }
 
